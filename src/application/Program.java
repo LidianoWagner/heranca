ca4d22359887cdf35821f89f81e2523c680d2d17
Lidiano Wagner <Lidiano.Xavier@hotmail.com>
2021-08-03 22:03:01 -0300

    Imprementação - DOWNCASTING/UPCASTING

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,16 +2,38 @@ package application;
 
 import entities.Account;
 import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        BusinessAccount account = new BusinessAccount();
+        Account acc = new Account(1001, "Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.00);
 
-        account.getBalance();
-        account.getNumber();
-        account.getHolder();
+        // UP CASTING
+
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.00);
+        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+        // DOWN CASTING
+
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+        acc4.loan(100.00);
+
+        if (acc3 instanceof BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.00);
+            System.out.println("Loan!");
+        }
+
+        if (acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
+
 
     }
 }
